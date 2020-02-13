1444a66942a86fae071ebd631e49e46ff7e48238
Matt Ball <matt@matt-ball.co.uk>
2020-02-13 14:06:38 -0800

    postman tests

---
+++ postman-tests/Create/New user/test.js
@@ -0,0 +1,43 @@
+const res = pm.response.json()
+const uniq = require('uniq')
+
+console.log(uniq([1, 1, 2, 1, 3]))
+
+pm.test('Status code is 200', () => {
+  pm.response.to.have.status(200)
+})
+
+pm.test('API response is successful', () => {
+  pm.expect(res.status).to.eql('success')
+})
+
+pm.test('Created user has correct type of user ID', () => {
+  pm.expect(res.id).to.be.a('string')
+})
+
+pm.test('Does not expose user password', () => {
+  pm.expect(res).to.not.have.property('password')
+})
+
+pm.test('Created user has all relevant fields', () => {
+  pm.expect(res).to.have.all.keys('status', 'id', 'firstName', 'lastName')
+})
+
+pm.test('Created user matches submitted user', () => {
+  const requestedUser = _.omit(rawToObject(pm.request.body.urlencoded.toString()), 'password')
+  const response = _.omit(res, ['id', 'status'])
+  pm.expect(requestedUser).to.eql(response)
+})
+
+pm.sendRequest({
+  url: `${pm.environment.get('url')}/delete?id=${res.id}`,
+  method: 'delete'
+})
+
+function rawToObject (str) {
+  return str.split('&').reduce((prev, curr, i, arr) => {
+    var p = curr.split('=')
+    prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1])
+    return prev
+  }, {})
+}
