6e3490788c23494607c90ca7f4665c3e02c3d468
Matt Ball <matt@matt-ball.co.uk>
2020-02-20 13:59:26 -0500

    lint

--- postman-tests/Create/New user/test.js
+++ postman-tests/Create/New user/test.js
@@ -1,40 +1,41 @@
-const res = pm.response.json();
+const _ = require('lodash')
+const res = pm.response.json()
 
 pm.test('Status code is 200', () => {
-    pm.response.to.have.status(200);
-});
+  pm.response.to.have.status(200)
+})
 
 pm.test('API response is successful', () => {
-    pm.expect(res.status).to.eql('success');
-});
+  pm.expect(res.status).to.eql('success')
+})
 
 pm.test('Created user has correct type of user ID', () => {
-    pm.expect(res.id).to.be.a('string');
-});
+  pm.expect(res.id).to.be.a('string')
+})
 
 pm.test('Does not expose user password', () => {
-    pm.expect(res).to.not.have.property('password');
-});
+  pm.expect(res).to.not.have.property('password')
+})
 
 pm.test('Created user has all relevant fields', () => {
-    pm.expect(res).to.have.all.keys('status', 'id', 'firstName', 'lastName');
-});
+  pm.expect(res).to.have.all.keys('status', 'id', 'firstName', 'lastName')
+})
 
 pm.test('Created user matches submitted user', () => {
-    const requestedUser = _.omit(rawToObject(pm.request.body.urlencoded.toString()), 'password');
-    const response = _.omit(res, ['id', 'status']);
-    pm.expect(requestedUser).to.eql(response);
-});
+  const requestedUser = _.omit(rawToObject(pm.request.body.urlencoded.toString()), 'password')
+  const response = _.omit(res, ['id', 'status'])
+  pm.expect(requestedUser).to.eql(response)
+})
 
 pm.sendRequest({
-    url: `${pm.environment.get('url')}/delete?id=${res.id}`,
-    method: 'delete'
-});
+  url: `${pm.environment.get('url')}/delete?id=${res.id}`,
+  method: 'delete'
+})
 
 function rawToObject (str) {
-    return str.split('&').reduce((prev, curr, i, arr) => {
-        var p = curr.split('=');
-        prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
-        return prev;
-    }, {});
+  return str.split('&').reduce((prev, curr, i, arr) => {
+    var p = curr.split('=')
+    prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1])
+    return prev
+  }, {})
 }
--- postman-tests/Read/Get users by first name/test.js
+++ postman-tests/Read/Get users by first name/test.js
@@ -1,45 +1,45 @@
-const res = pm.response.json();
-const id = pm.globals.get('id');
-const firstName = pm.variables.get('firstName');
+const res = pm.response.json()
+const id = pm.globals.get('id')
+const firstName = pm.variables.get('firstName')
 
 pm.test('Status code is 200', () => {
-    pm.response.to.have.status(200);
-});
+  pm.response.to.have.status(200)
+})
 
 pm.test('List of users is returned', () => {
-    pm.expect(res).to.be.an('array');
-});
+  pm.expect(res).to.be.an('array')
+})
 
 pm.test('All users have firstname: ' + firstName, () => {
-    res.forEach((val, i) => {
-        if (i > 0) {
-            pm.expect(val.firstName).to.eql(firstName);   
-        }
-    });
-});
+  res.forEach((val, i) => {
+    if (i > 0) {
+      pm.expect(val.firstName).to.eql(firstName)
+    }
+  })
+})
 
 pm.test('User passwords are not exposed', () => {
-    res.forEach((val, i) => {
-        if (i > 0) {
-            pm.expect(val).to.not.have.all.keys('password');   
-        }
-    });
-});
+  res.forEach((val, i) => {
+    if (i > 0) {
+      pm.expect(val).to.not.have.all.keys('password')
+    }
+  })
+})
 
 pm.test('User has all relevant fields and correct types', () => {
-    res.forEach((val, i) => {
-        if (i > 0) {
-            pm.expect(val.id).to.be.a('string');
-            pm.expect(val.firstName).to.be.a('string');
-            pm.expect(val.lastName).to.be.a('string');
-        }
-    });
-});
+  res.forEach((val, i) => {
+    if (i > 0) {
+      pm.expect(val.id).to.be.a('string')
+      pm.expect(val.firstName).to.be.a('string')
+      pm.expect(val.lastName).to.be.a('string')
+    }
+  })
+})
 
 // cleanup
-pm.globals.unset('id');
+pm.globals.unset('id')
 
 pm.sendRequest({
-    url: `${pm.environment.get('url')}/delete?id=${id}`,
-    method: 'delete'
-});
+  url: `${pm.environment.get('url')}/delete?id=${id}`,
+  method: 'delete'
+})
